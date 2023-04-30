084756684152881c5bb8c7d4f02dc08176e4f075
Roger <rogerdapalma@gmail.com>
2023-04-30 16:59:40 -0300

    Aprendendo estrutura de repetição

---
+++ Aula007/ex014.js
@@ -0,0 +1,19 @@
+/*
+var c = 1 
+do {
+    console.log(`C = ${c}`) 
+    c++ // c = c+1
+} while (c <= 6) // testa no final 
+*/
+
+var c1 = 1 
+while(c <= 6){ //testa no inicio
+    console.log(`C1 = ${c}`) 
+    c++ // c = c+1
+}
+
+/*
+console.log('Olá tudo bem ?') 
+console.log('Olá tudo bem ?') 
+console.log('Olá tudo bem ?') 
+*/
