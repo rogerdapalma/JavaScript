f29d97d61d2bf7c17ddfa8818300ab91fde8f3d7
Roger <rogerdapalma@gmail.com>
2023-05-06 21:35:33 -0300

    Continuação das estruturas de repetição

---
+++ Aula008/ex0015.js
@@ -0,0 +1,14 @@
+
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+//---------------
+console.log('vai começar...')
+for( c = 1;c <= 5;c++){ //mesma funcionalidade codigo acima 
+    console.log(c)
+}
+console.log('FIM!')
