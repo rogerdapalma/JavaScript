4889813544382508e94044ca1350456d6508ee95
Roger <rogerdapalma@gmail.com>
2023-06-01 15:46:01 -0300

    Aprendendo Funções

---
+++ Aula011/Funcao01.js
@@ -0,0 +1,11 @@
+function ParImpar(n) { //função
+    if (n % 2 == 0) {
+        return "Par!"
+    } else {
+        return 'impar!'
+    }
+}
+//let res = ParImpar(4)
+//console.log(res)
+
+console.log(ParImpar(223)) //chamada // 223 parametro
---
+++ Aula011/Funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0){ //se o n1 ou n2 nao for passado, se considera 0
+    return n1 + n2
+}
+
+//console.log(soma(2 , 5))   //n1 = 2 // n2 = 5 
+console.log(soma(2)) // function soma(n1 , n2){ //result NaN , leu como indefinido 
